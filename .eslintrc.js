5cf27282234140bbdf7545f15f3b9c9e7eed2e11
krhougs <gh@kt.je>
2021-05-21 19:56:04 +0000

    chore: nextjs setup

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'sort-imports-es6-autofix'],
+  rules: {
+    // 'no-use-before-define': 'off',
+    'no-unused-vars': 'off',
+    'sort-imports-es6-autofix/sort-imports-es6': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'multiple', 'single', 'all'],
+      },
+    ],
+    'react/no-unescaped-entities': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { BaseProvider, LightTheme, styled } from 'baseui'
+import { Provider as StyletronProvider } from 'styletron-react'
+import { styletron } from '../styletron'
+
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StyletronProvider value={styletron}>
+      <BaseProvider theme={LightTheme}>
+        <Component {...pageProps} />
+      </BaseProvider>
+    </StyletronProvider>
+  )
 }
 
 export default MyApp
