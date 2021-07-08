e4c9e6a2b09b065fb530d9a5d542e5db166c63f7
Jiacheng Zhang <gh@kt.je>
2021-07-08 19:47:15 +0000

    feat: estimatedTime

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,42 @@
 import { Card, StyledBody } from 'baseui/card'
 import { ListItem, ListItemLabel } from 'baseui/list'
-import { useMemo } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useQuery } from 'react-query'
 import Head from 'next/head'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime)
 
 const fetcher = ({ queryKey: [url] }) => fetch(url).then((r) => r.json())
 
 export default function Home() {
+  const [previousData, setPreviousData] = useState(null)
+  const [blockPerSec, setBlockPerSec] = useState(0)
   const { data } = useQuery('/api/fetch_status', fetcher, {
-    refetchInterval: 2000,
+    refetchInterval: 1000,
+    keepPreviousData: true,
   })
+
+  useEffect(() => {
+    const _p = previousData
+
+    if (previousData !== data) {
+      setPreviousData(data)
+      setBlockPerSec(data?.blobHeight - _p?.blobHeight || 0)
+    }
+  }, [data])
 
   const list = useMemo(
     () => Object.keys(data || {}).map((k) => [k, data[k]]),
     [data]
   )
+
+  const estimatedTime = useMemo(() => {
+    const rest = data?.knownHeight - data?.blobHeight || 0
+    const sec = parseInt(rest / blockPerSec) || 0
+    return dayjs().add(sec, 'second').fromNow(dayjs())
+  }, [blockPerSec, data?.blobHeight, data?.knownHeight])
 
   return (
     <div>
@@ -33,6 +55,16 @@ export default function Home() {
               </ListItemLabel>
             </ListItem>
           ))}
+          {!data?.hasReachedInitTarget ? (
+            <ListItem>
+              <ListItemLabel description={<code>{blockPerSec}</code>}>
+                blockPerSec
+              </ListItemLabel>
+              <ListItemLabel description={<code>{estimatedTime}</code>}>
+                estimatedTime
+              </ListItemLabel>
+            </ListItem>
+          ) : null}
         </StyledBody>
       </Card>
     </div>
