a8f2cd6dc7c1a40e1347c95ade8511f072a829ed
Jiacheng Zhang <gh@kt.je>
2021-07-29 15:57:41 +0800

    feat: pool

--- pages/pools.js
+++ pages/pools.js
@@ -3,6 +3,7 @@ import { AdaptedCheckbox } from 'baseui-final-form/checkbox'
 import { AdaptedInput } from 'baseui-final-form/input'
 import { Button, KIND, SHAPE, SIZE } from 'baseui/button'
 import { Card, StyledBody } from 'baseui/card'
+import { Delete, Overflow, Plus } from 'baseui/icon'
 import { FORM_ERROR } from 'final-form'
 import { Field, Form } from 'react-final-form'
 import { HeadingXLarge } from 'baseui/typography'
@@ -14,7 +15,13 @@ import {
   ModalHeader,
 } from 'baseui/modal'
 import { KIND as NOTIFICATION_KIND, Notification } from 'baseui/notification'
-import { Plus } from 'baseui/icon'
+import {
+  BooleanColumn,
+  NUMERICAL_FORMATS,
+  NumericalColumn,
+  StatefulDataTable,
+  StringColumn
+} from "baseui/data-table";
 import {
   pools as poolsAtom,
   updateAllLists,
@@ -22,11 +29,12 @@ import {
 } from '../atoms/mgmt'
 import { queryManager } from '../utils/query'
 import { useAtom } from 'jotai'
-import { useCallback, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
+import { useStyletron } from 'baseui'
 import { useUpdateAtom } from 'jotai/utils'
 import Head from 'next/head'
 
-const PoolModalForm = ({ initValues, onSubmit, setModalOpen }) => {
+const PoolModalForm = ({ initValues, onSubmit, setModalClose }) => {
   const validate = useCallback((values) => {
     const ret = {}
     if (!(parseInt(values.pid) >= 1)) {
@@ -45,7 +53,7 @@ const PoolModalForm = ({ initValues, onSubmit, setModalOpen }) => {
   return (
     <Form
       onSubmit={onSubmit}
-      initialValues={{ enabled: true }}
+      initialValues={initValues}
       validate={validate}
       render={({ handleSubmit, ...form }) => {
         return (
@@ -119,14 +127,10 @@ const PoolModalForm = ({ initValues, onSubmit, setModalOpen }) => {
                 <>Loading...</>
               ) : (
                 <>
-                  {' '}
-                  <ModalButton
-                    kind={KIND.tertiary}
-                    onClick={() => setModalOpen(false)}
-                  >
+                  <ModalButton kind={KIND.tertiary} onClick={setModalClose}>
                     Cancel
                   </ModalButton>
-                  <ModalButton type="submit">Add</ModalButton>
+                  <ModalButton type="submit">Save</ModalButton>
                 </>
               )}
             </ModalFooter>
@@ -134,6 +138,82 @@ const PoolModalForm = ({ initValues, onSubmit, setModalOpen }) => {
         )
       }}
     />
+  )
+}
+
+const PoolRowEditModal = ({ currentRow, clearCurrentRow }) => {
+  const modalOpen = useMemo(() => !!currentRow, [currentRow])
+  const updateLists = useUpdateAtom(updateAllLists)
+  const initValues = useMemo(
+    () =>
+      currentRow
+        ? {
+            pid: currentRow.data.pid,
+            name: currentRow.data.name,
+            polkadotJson: currentRow.data.owner.polkadotJson,
+            enabled: currentRow.data.enabled,
+          }
+        : {},
+    [currentRow]
+  )
+  const submit = useCallback(
+    async (values) => {
+      try {
+        const { lifecycleManagerStateUpdate } = await queryManager({
+          queryKey: [
+            {
+              requestUpdatePool: {
+                items: [
+                  {
+                    id: { uuid: currentRow.data.uuid },
+                    pool: {
+                      name: values.name,
+                      pid: values.pid,
+                      enabled: values.enabled,
+                      owner: {
+                        mnemonic: values.mnemonic,
+                        polkadotJson: values.polkadotJson,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          ],
+        })
+        updateLists({
+          pools: lifecycleManagerStateUpdate.pools || [],
+          workers: lifecycleManagerStateUpdate.workers || [],
+        })
+        clearCurrentRow()
+        return {}
+      } catch (e) {
+        if (e.isProtoError) {
+          return {
+            [FORM_ERROR]: e.extra,
+          }
+        }
+        return {
+          [FORM_ERROR]: e,
+        }
+      }
+    },
+    [currentRow]
+  )
+  return (
+    <Modal
+      onClose={clearCurrentRow}
+      isOpen={modalOpen}
+      unstable_ModalBackdropScroll={true}
+      closeable={false}
+    >
+      <ModalHeader>Edit Pool</ModalHeader>
+      <PoolModalForm
+        setModalClose={clearCurrentRow}
+        initValues={initValues}
+        onSubmit={submit}
+      />
+    </Modal>
   )
 }
 
@@ -201,8 +281,8 @@ const CreatePoolModalWithButton = () => {
       >
         <ModalHeader>Add Pool</ModalHeader>
         <PoolModalForm
-          setModalOpen={setModalOpen}
-          initValues={{}}
+          setModalClose={() => setModalOpen(false)}
+          initValues={{ enabled: true }}
           onSubmit={submit}
         />
       </Modal>
@@ -210,8 +290,103 @@ const CreatePoolModalWithButton = () => {
   )
 }
 
+const listColumn = [
+  NumericalColumn({
+    title: 'PID',
+    format: NUMERICAL_FORMATS.DEFAULT,
+    mapDataToValue: (data) => parseInt(data.pid),
+  }),
+  StringColumn({
+    title: 'Name',
+    mapDataToValue: (data) => data.name,
+  }),
+  BooleanColumn({
+    title: 'Enabled',
+    mapDataToValue: (data) => !!data.enabled,
+  }),
+  StringColumn({
+    title: 'Owner Account(Phala)',
+    mapDataToValue: (data) => data.owner.ss58Phala,
+  }),
+  StringColumn({
+    title: 'Owner Account(Polkadot)',
+    mapDataToValue: (data) => data.owner.ss58Polkadot,
+  }),
+  StringColumn({
+    title: 'UUID',
+    mapDataToValue: (data) => data.uuid,
+  }),
+]
+
 const PoolsList = ({ pools }) => {
-  return <pre>{JSON.stringify(pools, null, 2)}</pre>
+  const [currentRow, setCurrentRow] = useState(null)
+  const [css] = useStyletron()
+  const updateLists = useUpdateAtom(updateAllLists)
+  const deleteRow = useCallback(
+    async ({ row }) => {
+      if (!window.confirm(`Delete pool #${row.data.pid}?`)) {
+        return
+      }
+      const { lifecycleManagerStateUpdate } = await queryManager({
+        queryKey: [
+          {
+            requestUpdatePool: {
+              items: [
+                {
+                  id: { uuid: row.data.uuid },
+                  pool: {
+                    ...row.data,
+                    deleted: true,
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      })
+      window.alert('Success!')
+      updateLists({
+        pools: lifecycleManagerStateUpdate.pools || [],
+        workers: lifecycleManagerStateUpdate.workers || [],
+      })
+    },
+    [updateLists]
+  )
+  const rowActions = useMemo(
+    () => [
+      {
+        label: 'Delete',
+        onClick: deleteRow,
+        renderIcon: ({ size }) => <Delete size={size} />,
+      },
+      {
+        label: 'Edit',
+        onClick: ({ row }) => setCurrentRow(row),
+        renderIcon: ({ size }) => <Overflow size={size} />,
+      },
+    ],
+    [deleteRow, setCurrentRow]
+  )
+  const rows = useMemo(
+    () => pools.map((i) => ({ id: i.pid, data: i })),
+    [pools]
+  )
+  return (
+    <>
+      <div className={css({ height: 'calc(100vh - 270px)' })}>
+        <StatefulDataTable
+          columns={listColumn}
+          rows={rows}
+          rowActions={rowActions}
+          resizableColumnWidths
+        />
+      </div>
+      <PoolRowEditModal
+        currentRow={currentRow}
+        clearCurrentRow={() => setCurrentRow(null)}
+      />
+    </>
+  )
 }
 
 const PoolsPage = () => {
